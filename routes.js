4addd83138af4c5c0ec85d9784606cdaa0f5522b
andraderafa <andraderafa@users.github.com>
2020-11-11 12:46:18 -0300

    Base
    Application base

---
+++ routes.js
@@ -0,0 +1,9 @@
+/*
+ * A ROTA ESCOLHE O CONTROLLER, 
+ * E O CONTROLLER ESCOLHE O MODEL E A VIEW QUE VAI RENDERIZAR 
+ */
+
+const express = require('express');
+const route = express.Router();
+
+module.exports = route;
---
+++ server.js
@@ -0,0 +1,62 @@
+// VARIAVEIS DE AMBIENTE
+require('dotenv').config();
+
+// SERVIDOR 
+const express = require('express');
+const app = express();
+
+// ARQUITETURA PARA O BANCO DE DADOS
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.CONNECTION_STRING, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.emit('ready'))
+  .catch(e => console.log(e));
+
+// COOKIES E SALVAMENTOS
+const session = require('express-session');
+const MongoStorage = require('connect-mongo')(session);
+const flash = require('connect-flash');
+
+// CAMINHOS E ROTAS
+const routes = require('routes');
+const path = require('path');
+
+// SEGURANÇA
+const helmet = require('helmet');
+const csrf = require('csurf');
+app.use(helmet());
+
+// POSTAGEM DE FORMULÁRIO
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// ARQUIVOS ESTATICOS QUE DEVEM SER ACESSADOS DIRETAMENTE
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+// CONFIGURAÇÕES DE SESSÃO
+const sessionOptions = session({
+  secret: 'mirapalheta',
+  store: new MongoStorage({ mongooseConnection: mongoose.connection }),
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+    httpOnly: true
+  }
+});
+
+app.use(sessionOptions);
+app.use(flash());
+
+// PAGINAS HTML - EJS
+app.set('views', path.resolve(__dirname, 'src', 'views'));
+app.set('view engine', 'ejs'); // engine para renderizar html
+
+app.use(csrf());
+
+app.use(routes);
+
+// LISTENER APÓS CONECTAR NO BANCO DE DADOS
+app.on('ready', () => {
+  app.listen(5500, () => console.log("Servidor rodando na porta 5500: http://localhost:5500"))
+});
